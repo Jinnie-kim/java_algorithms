0ee0111c0d139b458ae3076780bf6b1a6f21334e
Jinnie <hjk7639@gmail.com>
2024-03-15 21:59:59 +0900

    031524 - BaekJoon 10869

---
+++ src/BaekJoon/Day031510869.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Day031510869 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
