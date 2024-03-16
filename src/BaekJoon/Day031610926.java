fd06a2121189449d618437c39e8ffedf12849701
Jinnie <hjk7639@gmail.com>
2024-03-16 19:39:02 +0900

    031624 - BaekJoon 10926

---
+++ src/BaekJoon/Day031610926.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Day031610926 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        System.out.println(name + "??!");
+    }
+}
