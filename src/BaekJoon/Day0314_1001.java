67da2992c5469094e9c26e57009e4e9b4c02c9fd
Jinnie <hjk7639@gmail.com>
2024-03-17 11:04:58 +0900

    Fix: change file name format

--- src/BaekJoon/Day0314_1001.java
+++ src/BaekJoon/Day0314_1001.java
@@ -2,17 +2,14 @@ package BaekJoon;
 
 import java.util.Scanner;
 
-public class Day031510869 {
+public class Day0314_1001 {
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(a + b);
         System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
     }
 }
