d6205cd300b3eaa259bfb3e63a53655cf4bb5827
Jinnie <hjk7639@gmail.com>
2024-03-17 10:46:52 +0900

    031724 - BaekJoon 10430, 18108

---
+++ src/BaekJoon/Day0317_10430.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Day0317_10430 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+    }
+}
---
+++ src/BaekJoon/Day0317_18108.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Day0317_18108 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
+
+        System.out.println(age - 543);
+    }
+}
