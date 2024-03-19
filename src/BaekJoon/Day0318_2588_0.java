75b0436dc9bbf510d85b987a2f1aa4bac69d1649
Jinnie <hjk7639@gmail.com>
2024-03-19 20:13:06 +0900

    Fix: change file name date

--- src/BaekJoon/Day0318_2588_0.java
+++ src/BaekJoon/Day0318_2588_0.java
@@ -2,7 +2,7 @@ package BaekJoon;
 
 import java.util.Scanner;
 
-public class Day0317_2588_0 {
+public class Day0318_2588_0 {
     // 하나로 입력된 문자(숫자)를 분리하여 연산을 할 수 있는가?
 
     public static void main(String args[]) {
--- src/BaekJoon/Day0318_2588_1.java
+++ src/BaekJoon/Day0318_2588_1.java
@@ -3,9 +3,8 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
-public class Day0317_2588_1 {
+public class Day0318_2588_1 {
     // 하나로 입력된 문자(숫자)를 분리하여 연산을 할 수 있는가?
 
     public static void main(String args[]) throws IOException {
