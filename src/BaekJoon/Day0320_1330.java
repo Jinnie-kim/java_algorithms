fe54c3474fbe869a6f6e7500fb2d064127ee90a7
Jinnie <hjk7639@gmail.com>
2024-03-20 20:06:13 +0900

    032024 - BaekJoon 1330

---
+++ src/BaekJoon/Day0320_1330.java
@@ -0,0 +1,26 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day0320_1330 {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] strs = br.readLine().split(" ");
+
+        int a = Integer.parseInt(strs[0]);
+        int b = Integer.parseInt(strs[1]);
+
+        if(a > b) {
+            System.out.println(">");
+        } else if(a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+
+    }
+}
