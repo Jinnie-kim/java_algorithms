c88af83e908782a34167bdfdbb1c09d7cc31b901
Jinnie <hjk7639@gmail.com>
2024-03-20 20:01:03 +0900

    032024 - BaekJoon 10171, 10172

---
+++ src/BaekJoon/Day0320_10171.java
@@ -0,0 +1,9 @@
+package BaekJoon;
+
+public class Day0320_10171 {
+
+    public static void main(String args[]) {
+
+        System.out.println("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|");
+    }
+}
---
+++ src/BaekJoon/Day0320_10172.java
@@ -0,0 +1,9 @@
+package BaekJoon;
+
+public class Day0320_10172 {
+
+    public static void main(String args[]) {
+
+        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+    }
+}
