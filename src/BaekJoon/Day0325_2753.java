a0e65657151fb46ee8a1e906273f95cf68081034
Jinnie <hjk7639@gmail.com>
2024-03-25 20:31:12 +0900

    032524 - BaekJoon 2753, 14681

---
+++ src/BaekJoon/Day0325_2753.java
@@ -0,0 +1,26 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day0325_2753 {
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        int year = Integer.parseInt(str);
+
+        if(year % 4 == 0 && year % 100 != 0) {
+            System.out.println(1);
+        } else if(year % 4 == 0 && year % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+    }
+}
